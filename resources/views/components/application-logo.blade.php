86c7ea6b1fd0cd69c9c9bf913cc3ce0d0d9c429d
Tatiana <tatianaoe83@gmail.com>
2025-07-23 17:36:01 -0600

    Cambio_logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo.svg') }}" alt="Logo de la aplicación" {{ $attributes }}>
+<img src="{{ asset('images/logo.svg') }}" alt="Logo de la aplicación" {{ $attributes }} style="width: 130px; height: 130px;">
